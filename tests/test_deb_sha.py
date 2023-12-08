76eff245d0a5cbc0913f494140df479323cabe88
Lucian I. Last <li@last.nl>
2023-12-08 13:45:47 +0100

    Add size test and indent with tabs

--- tests/test_deb_sha.py
+++ tests/test_deb_sha.py
@@ -1,27 +1,37 @@
+"""Module providing hash functions."""
 import hashlib
-import yaml
 from urllib.request import urlretrieve
+import os
+import yaml
 
 def test_deb_sha():
-    # Read yaml file
-    with open('../com.synology.SynologyDrive.yaml', 'r', encoding="utf-8") as fileYaml:
-        appYaml = yaml.safe_load(fileYaml)
+	"""Test that the deb file has the correct sha256 and size as discribed"""
 
-        # Retrieve url and sha
-        debUrl = ""
-        debSha256 = ""
-        sources = appYaml['modules'][0]["sources"]
-        for source in sources:
-            if "filename" in source and source["filename"] == "synology-drive.deb":
-                debUrl = source["url"]
-                debSha256 = source["sha256"]
-                break
-        
-        # Download deb
-        urlretrieve(debUrl, ".synology-drive.deb")
+	# Read yaml file
+	with open('../com.synology.SynologyDrive.yaml', 'r', encoding="utf-8") as file_yaml:
+		app_yaml = yaml.safe_load(file_yaml)
 
-        # Open deb
-        with open('.synology-drive.deb', 'rb') as fileDeb:
-            b = fileDeb.read()
-            readable_hash = hashlib.sha256(b).hexdigest()
-            assert readable_hash == debSha256
+		# Retrieve url and sha
+		deb_url = ""
+		deb_sha_256 = ""
+		deb_size = 0
+		sources = app_yaml['modules'][0]["sources"]
+		for source in sources:
+			if "filename" in source and source["filename"] == "synology-drive.deb":
+				deb_url = source["url"]
+				deb_sha_256 = source["sha256"]
+				deb_size = source["size"]
+				break
+
+		# Download deb
+		urlretrieve(deb_url, ".synology-drive.deb")
+
+		# Test file size deb
+		size = os.path.getsize(".synology-drive.deb")
+		assert size == deb_size
+
+		# Test hash deb
+		with open('.synology-drive.deb', 'rb') as file_deb:
+			b = file_deb.read()
+			readable_hash = hashlib.sha256(b).hexdigest()
+			assert readable_hash == deb_sha_256
